729a13493614ba05eb4803f7dac08028caa25556
JZSweet <sweet_zhaozhao@live.cn>
2021-04-23 13:19:57 -0700

    add pic for blog page

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import { Carousel,Container,Row,Col } from "react-bootstrap";
-import sky1 from "./1.jpg"
+import { Carousel, Container, Row, Col } from "react-bootstrap";
+import sky1 from "./1.jpg";
+import sky2 from "./2.jpg";
+import sky3 from "./3.JPG";
 
 class Blog extends Component {
   render() {
     return (
       <Container>
         <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
+          <Col>Some of my works and life outside of coding.</Col>
+          <Col>Photograph credit:JZ</Col>
         </Row>
         <Row>
           <Col>
@@ -19,34 +21,22 @@ class Blog extends Component {
                   src={sky1}
                   alt="First slide"
                 />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
               </Carousel.Item>
+
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src={sky1}
+                  src={sky2}
                   alt="Second slide"
                 />
-
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
               </Carousel.Item>
+
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src={sky1}
+                  src={sky3}
                   alt="Third slide"
                 />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
           </Col>
